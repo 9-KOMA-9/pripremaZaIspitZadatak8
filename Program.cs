1eb0d261ff0c20b2f480f9f0d7b107109a868428
9-KOMA-9 <145533890+9-KOMA-9@users.noreply.github.com>
2023-11-13 21:05:41 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pripremaZaIspitZadatak8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("unesite bilo koji broj koji zelite: ");
+            int a =  Convert.ToInt32(Console.ReadLine());
+            if(a % 4 == 0 )
+            {
+                Console.WriteLine("broj je djeljiv sa 4");
+            }
+            if (a % 6 == 0)
+            {
+                Console.WriteLine("broj je djeljiv sa 6");
+            }
+            if (a % 4 == 0 && a % 6 == 0)
+            {
+                Console.WriteLine("broj je djeljiv sa 4 i sa 6");
+            }
+            if (a % 4 != 0 && a % 6 != 0)
+            {
+                Console.WriteLine("broj nije djeljiv sa 4 ni sa 6");
+            }
+            Console.ReadKey();
+        }
+    }
+}
